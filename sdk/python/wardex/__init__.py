2f5cda667b9a771df63f11106a78815d9cdaa027
pinkysworld <85413447+pinkysworld@users.noreply.github.com>
2026-05-06 19:24:38 +0200

    Align Python SDK runtime version

--- sdk/python/wardex/__init__.py
+++ sdk/python/wardex/__init__.py
@@ -13,7 +13,7 @@ from wardex.exceptions import (
     ServerError,
 )
 
-__version__ = "1.0.0"
+__version__ = "1.0.5"
 __all__ = [
     "CommandCenterLaneResponse",
     "CommandCenterSummaryResponse",
